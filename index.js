003bb6aa498e114f6943caaf175283351b97e2e8
GalinaCheren <galinacheren8@gmail.com>
2022-10-08 22:27:57 +0300

    HomeWork4 - arithmetic average

---
+++ index.js
@@ -0,0 +1,4 @@
+let fistNumber = prompt("Please, write your fist number:");
+let secondNumber = prompt("Please, write your second number:");
+let thirdNumber = prompt("Please, write your third number:");
+alert((Number(fistNumber) + Number(secondNumber) + Number(thirdNumber)) / 3);
